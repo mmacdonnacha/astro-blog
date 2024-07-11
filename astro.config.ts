6e4255e6587218359d9bafdaea7fffcd186e078d
Micheal Mac Donnacha <micheal.macdonnacha13@gmail.com>
2024-07-11 23:31:47 +0100

    more changes 2

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+  site: `${SITE.website}/${SITE.base}`,
   base: SITE.base,
   integrations: [
     tailwind({
